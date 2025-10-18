292d31bc51bb8ff4dd71af5fa9eeeadd5e7f4b56
daun <wjdekdns0218@gmail.com>
2025-10-18 17:19:39 +0900

    feat(calculator): 빈 문자열 입력 시 0 반환 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,6 +2,10 @@ package calculator;
 
 public class StringCalculator {
     public static int calculate(String input) {
+        if(input == null || input.isEmpty()) {
+            return 0;
+        }
+
         return 0;
     }
 }
