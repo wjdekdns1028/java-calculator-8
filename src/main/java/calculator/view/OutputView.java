c114234fe1afdd11a62b44b79d88c95fc37eca54
daun <wjdekdns0218@gmail.com>
2025-10-20 23:32:42 +0900

    feat(view): OutputView 추가 및 계산 결과 출력 기능 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private OutputView() {}
+
+    public static void printResult(int result) {
+        System.out.println("결과 : "  + result);
+    }
+}
