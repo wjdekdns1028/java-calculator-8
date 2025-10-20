b175cc1857c5bff84707d2700db6f8c3f41f9a64
daun <wjdekdns0218@gmail.com>
2025-10-20 23:34:51 +0900

    chore(legacy): 기존 StringCalculator 주석 처리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,40 +1,40 @@
-package calculator;
-
-public class StringCalculator {
-    public static int calculate(String input) {
-        if(input == null || input.isEmpty()) {
-            return 0;
-        }
-
-        input = input.replace("\\n", "\n");
-
-        String delimiter = "[,:]"; // 기본 구분자
-
-        if(input.startsWith("//")){
-            int delimiterIndex = input.indexOf("\n");
-            delimiter = input.substring(2, delimiterIndex);
-            input = input.substring(delimiterIndex + 1);
-        }
-
-        String[] tokens = input.split(delimiter);
-        int sum = 0;
-
-        for(String token : tokens){
-            validateNumber(token);
-            sum += Integer.parseInt(token);
-        }
-
-        return sum;
-    }
-
-    private static void validateNumber(String token){
-        if(!token.matches("-?\\d+")){
-            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
-        }
-
-        int number = Integer.parseInt(token);
-        if(number < 0){
-            throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + number);
-        }
-    }
-}
+//package calculator;
+//
+//public class StringCalculator {
+//    public static int calculate(String input) {
+//        if(input == null || input.isEmpty()) {
+//            return 0;
+//        }
+//
+//        input = input.replace("\\n", "\n");
+//
+//        String delimiter = "[,:]"; // 기본 구분자
+//
+//        if(input.startsWith("//")){
+//            int delimiterIndex = input.indexOf("\n");
+//            delimiter = input.substring(2, delimiterIndex);
+//            input = input.substring(delimiterIndex + 1);
+//        }
+//
+//        String[] tokens = input.split(delimiter);
+//        int sum = 0;
+//
+//        for(String token : tokens){
+//            validateNumber(token);
+//            sum += Integer.parseInt(token);
+//        }
+//
+//        return sum;
+//    }
+//
+//    private static void validateNumber(String token){
+//        if(!token.matches("-?\\d+")){
+//            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+//        }
+//
+//        int number = Integer.parseInt(token);
+//        if(number < 0){
+//            throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + number);
+//        }
+//    }
+//}
